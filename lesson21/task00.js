3dd603e7ef33e876658dc7642f7225bcf28741d0
ilyalifsh <ilyalifsh@gmail.com>
2026-08-24 21:24:23 +0300

    lesson21 final commit

--- lesson21/task00.js
+++ lesson21/task00.js
@@ -61,11 +61,11 @@ function comparator(a,b){
     }
 } 
 
-const comparator2 = function(a2,b2){
- if (a2.length>b2.length){
+const comparator2 = function(a,b){
+ if (a.length>b.length){
         return 1;
     }
-     else if (a2.length<b2.length){
+     else if (a.length<b.length){
         return -1;
     }
     else {
@@ -73,11 +73,11 @@ const comparator2 = function(a2,b2){
     }
 }
 
-const comparator3 =(a3,b3) =>{
-     if (a3.length>b3.length){
+const comparator3 =(a,b) =>{
+     if (a.length>b.length){
         return 1;
     }
-     else if (a3.length<b3.length){
+     else if (a.length<b.length){
         return -1;
     }
     else {
@@ -91,7 +91,7 @@ console.log(comparator3(massive[0],massive[1]));
 
 //#3
 
-function filter(massive,comparator2){
+function filter(massive,comparator){
     let big = massive[0];
     for (let i = 0; i < massive.length; i++) {
         if (comparator(massive[i],big)===1){
